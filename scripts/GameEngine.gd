860e699ddce9a11d5b8c54ff1803135568b4edb2
Rain Babauta <rainbabauta1@gmail.com>
2023-12-08 16:09:02 -0800

    Blueprint to dict
    Not done but this is the start of converting an XML blueprint into a dict which can then be converted into a node.

--- scripts/GameEngine.gd
+++ scripts/GameEngine.gd
@@ -32,24 +32,49 @@ class GameObjectFactory:
 		blueprints = load_blueprints()
 	
 	func load_blueprints() -> Dictionary:
-		var blueprint_dict: Dictionary = {}
+		var blueprint_dict: Dictionary = {
+			"Blueprint": {},
+			"Component": {},
+		}
 		var parser = XMLParser.new()
 		parser.open(BLUEPRINTS_PATH)
 		while parser.read() != ERR_FILE_EOF:
+			# if parsing NODE_ELEMENT of name Blueprint create new dict inside 
+			# blueprint_dict named after the blueprint name
+			
+			# add component details to a new dict until parsing NODE_ELEMENT_END 
+			# of name Blueprint set dict inside blueprint_dict
+			
+			
+			if parser.get_node_type() == XMLParser.NODE_ELEMENT_END:
+				print(parser.get_node_name())
 			if parser.get_node_type() == XMLParser.NODE_ELEMENT:
 				var node_name = parser.get_node_name()
 				var attributes_dict = {}
-				for idx in range(parser.get_attribute_count()):
-					attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
-				# print("The ", node_name, " element has the following attributes: ", attributes_dict)
-				blueprint_dict[node_name] = attributes_dict
 
+				if node_name == "Blueprint":
+					for idx in parser.get_attribute_count():
+						attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
+					blueprint_dict[node_name] = attributes_dict
+
+				if node_name == "Component":
+					var component_name = parser.get_attribute_value(0)
+					for idx in range(1, parser.get_attribute_count()):
+						attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
+					blueprint_dict[node_name][component_name] = attributes_dict
+
+				# print("The ", node_name, " element has the following attributes: ", attributes_dict)
+				# blueprint_dict[node_name].append(attributes_dict)
+
+		print(blueprint_dict)
 		return blueprint_dict
 
 	func create_object(blueprint: String) -> GameObject:
 		var new_game_object: GameObject = GameObject.new()
 		# TODO: find blueprint in blueprints
+		
 		# TODO: instantiate all component based on what the found blueprint has w/ params
+
 		return new_game_object
 
 	
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,8 +4,8 @@ func _ready():
 	generate_nodes()
 
 func generate_nodes() -> void:
-	var game_engine = GameEngine.new()
-	var physical_object = game_engine.GameObjectFactory.create_object("PhysicalObject")
+	var game_object_factory = GameEngine.GameObjectFactory.new()
+	var physical_object = game_object_factory.create_object("PhysicalObject")
 	var physical_object_sprite_node = Sprite2D.new()
 	var physical_object_render_component = physical_object.components.get("Render")
 
