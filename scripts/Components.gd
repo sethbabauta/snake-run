ac1a2a52d2920986fda58f8ffc6b832f275c824f
Rain Babauta <rainbabauta1@gmail.com>
2023-12-15 14:37:30 -0800

    Cleanup 2
    Ready for production*
    
    *with johngoals example. should remove before prod

--- scripts/Components.gd
+++ scripts/Components.gd
@@ -4,11 +4,20 @@ class_name Components
 
 
 class Physics extends Component:
-	enum Directions {N=1, S=-1, E=1, W=-1}
+    enum Directions {N=1, S=-1, E=1, W=-1}
 
-	var speed: int = 0
-	var direction: Directions = Directions.N
+    var speed: int = 0
+    var direction: Directions = Directions.N
 
 
 class Render extends Component:
-	var texture: String = ""
+    var texture: String = ""
+
+    func fire_event(event: Event) -> Event:
+        if event.id == "CreateSpriteNode":
+            var sprite_node:= Sprite2D.new()
+            sprite_node.position = event.parameters.get("start_position")
+            sprite_node.texture = load(self.texture)
+            event.parameters.get("parent_node").add_child(sprite_node)
+
+        return event
--- scripts/GameEngine.gd
+++ scripts/GameEngine.gd
@@ -7,166 +7,204 @@ const SPRITES_PATH = "res://Sprites/"
 
 
 class Event:
-	var id: String
-	var params: Dictionary
+    var id: String
+    var parameters: Dictionary
+
+    func _init(id: String = "NoID", parameters: Dictionary = {}) -> void:
+        self.id = id
+        self.parameters = parameters
 
 
 class Component:
-	"""
-	Do not fire events to components directly, always fire events to GameObjects
-	and let the GameObject fire the event to each of its components
-	"""
-	var name: String
-	var params: Dictionary = {}
+    """
+    Do not fire events to components directly, always fire events to
+    GameObjects and let the GameObject fire the event to each of its components
+    """
+    var name: String
 
-	func fire_event(Event) -> bool:
-		return true
+    # return event so that it's clear that event is changing in place
+    func fire_event(event: Event) -> Event:
+        return event
 
 
 class GameObject:
-	var name: String
-	var components: Dictionary = {}
+    var name: String
+    var components: Dictionary
 
-	func _init(name="NoName") -> void:
-		name = name
+    func _init(name: String = "NoName", components: Dictionary = {}) -> void:
+        self.name = name
+        self.components = components
 
 
-	func fire_event(Event) -> bool:
+    # return event so that it's clear that event is changing in place
+    func fire_event(event: Event) -> Event:
+        for component in self.components.values():
+            event = component.fire_event(event)
 
-		for component in components:
-			component.fire_event(Event)
-
-		return true
+        return event
 
 
 class GameObjectFactory:
-	var blueprints: Dictionary = {}
+    var blueprints: Dictionary = {}
 
-	func _init() -> void:
-		blueprints = load_blueprints()
-
-
-	func _get_blueprint_params(parser: XMLParser) -> Dictionary:
-		var blueprint_params: Dictionary = {}
-
-		for idx in parser.get_attribute_count():
-			blueprint_params[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
-
-		return blueprint_params
+    func _init() -> void:
+        self.blueprints = load_blueprints()
 
 
-	func _get_inherited_components(blueprint: Dictionary, blueprints: Dictionary) -> Dictionary:
-		var blueprint_to_inherit: String = blueprint["params"].get("Inherits")
-		assert(
-			blueprints.has(blueprint_to_inherit),
-			"Blueprint Inheritence error: Inherited blueprint not found."
-		)
+    func _get_blueprint_parameters(parser: XMLParser) -> Dictionary:
+        var blueprint_parameters: Dictionary = {}
 
-		var new_blueprint: Dictionary = blueprint.duplicate(true)
-		new_blueprint["components"] = blueprints.get(blueprint_to_inherit).get("components").duplicate(true)
+        for idx in parser.get_attribute_count():
+            blueprint_parameters[parser.get_attribute_name(idx)] = (
+                    parser.get_attribute_value(idx)
+            )
 
-		return new_blueprint
+        return blueprint_parameters
 
 
-	func _format_blueprint(parser: XMLParser, blueprints: Dictionary) -> Dictionary:
-		var blueprint: Dictionary = {
-			"params": {},
-			"components": {},
-		}
-		var blueprint_params: Dictionary = _get_blueprint_params(parser)
-		blueprint["params"] = blueprint_params
+    func _get_inherited_components(
+            blueprint: Dictionary,
+            blueprints: Dictionary,
+    ) -> Dictionary:
+        var blueprint_to_inherit: String = blueprint["parameters"].get("Inherits")
+        assert(
+            blueprints.has(blueprint_to_inherit),
+            "Blueprint Inheritence error: Inherited blueprint not found."
+        )
 
-		if blueprint["params"].has("Inherits"):
-			blueprint = _get_inherited_components(blueprint, blueprints)
+        var new_blueprint: Dictionary = blueprint.duplicate(true)
+        new_blueprint["components"] = (
+                blueprints
+                .get(blueprint_to_inherit)
+                .get("components")
+                .duplicate(true)
+        )
 
-		parser.read()
-		var node_name: String = parser.get_node_name()
-		var loop_count: int = 0
-
-		while not (parser.get_node_type() == XMLParser.NODE_ELEMENT_END and node_name == "Blueprint") and loop_count < 1000:
-			var component_params: Dictionary = {}
-			var component_name: String = parser.get_attribute_value(0)
-
-			for idx in range(1, parser.get_attribute_count()):
-				component_params[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
-			blueprint["components"][component_name] = component_params
-
-			parser.read()
-			node_name = parser.get_node_name()
-			loop_count += 1
-
-		return blueprint
+        return new_blueprint
 
 
-	func load_blueprints() -> Dictionary:
-		"""
-		Converts XML Blueprints into dict like:
-		{
-			bp1: {
-				params: { Name: "bp1" },
-				components: {
-					comp1: { p1: "p1", p2: "p2" },
-					comp2: { p1: "p1", p2: "p2" },
-				},
-			},
-			bp2: {
-				params: { Name: "bp2", Inherits: "bp1" },
-				components: {
-					comp3: { p1: "p1", p2: "p2" },
-				},
-			},
-		}
-		"""
-		var blueprints: Dictionary = {}
-		var parser: XMLParser = XMLParser.new()
-		parser.open(BLUEPRINTS_PATH)
+    func _format_blueprint(parser: XMLParser, blueprints: Dictionary) -> Dictionary:
+        const MAX_LOOPS = 1000
 
-		while parser.read() != ERR_FILE_EOF:
-			var node_name: String = parser.get_node_name()
-			assert(node_name == "Blueprint", "Blueprints formatting error: Blueprint not found.")
+        var blueprint: Dictionary = {
+            "parameters": {},
+            "components": {},
+        }
+        var blueprint_parameters: Dictionary = _get_blueprint_parameters(parser)
+        blueprint["parameters"] = blueprint_parameters
 
-			var formatted_blueprint: Dictionary = _format_blueprint(parser, blueprints)
-			var blueprint_name: String = formatted_blueprint["params"]["Name"]
+        if blueprint["parameters"].has("Inherits"):
+            blueprint = _get_inherited_components(blueprint, blueprints)
 
-			blueprints[blueprint_name] = formatted_blueprint
+        parser.read()
+        var node_name: String = parser.get_node_name()
+        var loop_count: int = 0
 
-		return blueprints
+        while (
+                not (parser.get_node_type() == XMLParser.NODE_ELEMENT_END
+                and node_name == "Blueprint")
+                and loop_count < MAX_LOOPS
+        ):
+            var component_parameters: Dictionary = {}
+            var component_name: String = parser.get_attribute_value(0)
 
+            for idx in range(1, parser.get_attribute_count()):
+                component_parameters[parser.get_attribute_name(idx)] = (
+                        parser.get_attribute_value(idx)
+                )
+            blueprint["components"][component_name] = component_parameters
 
-	func _set_component_params(new_component: Variant, component_params: Dictionary) -> void:
-		for param_name in component_params.keys():
-			if param_name == "texture":
-				component_params[param_name] = SPRITES_PATH + component_params[param_name]
-			if param_name in new_component:
-				new_component[param_name] = component_params[param_name]
+            parser.read()
+            node_name = parser.get_node_name()
+            loop_count += 1
+
+        return blueprint
 
 
-	func _create_components(blueprint_components: Dictionary) -> Dictionary:
-		var components_class: Components = Components.new()
-		var components: Dictionary = {}
+    func load_blueprints() -> Dictionary:
+        """
+        Converts XML Blueprints into dict like:
+        {
+            bp1: {
+                parameters: { Name: "bp1" },
+                components: {
+                    comp1: { p1: "p1", p2: "p2" },
+                    comp2: { p1: "p1", p2: "p2" },
+                },
+            },
+            bp2: {
+                parameters: { Name: "bp2", Inherits: "bp1" },
+                components: {
+                    comp3: { p1: "p1", p2: "p2" },
+                },
+            },
+        }
+        """
+        var blueprints: Dictionary = {}
+        var parser: XMLParser = XMLParser.new()
+        parser.open(BLUEPRINTS_PATH)
 
-		for idx in blueprint_components.size():
-			var current_component_name: String = blueprint_components.keys()[idx]
-			assert(current_component_name in components_class)
+        while parser.read() != ERR_FILE_EOF:
+            var node_name: String = parser.get_node_name()
+            assert(
+                    node_name == "Blueprint",
+                    "Blueprints formatting error: Blueprint not found.",
+            )
 
-			var current_params: Dictionary = blueprint_components.values()[idx]
-			var new_component: Variant = components_class[current_component_name].new()
-			_set_component_params(new_component, current_params)
+            var formatted_blueprint: Dictionary = _format_blueprint(parser, blueprints)
+            var blueprint_name: String = formatted_blueprint["parameters"]["Name"]
 
-			components[current_component_name] = new_component
+            blueprints[blueprint_name] = formatted_blueprint
 
-		return components
+        return blueprints
 
 
-	func create_object(blueprint_name: String) -> GameObject:
-		assert(blueprints.has(blueprint_name), "Create Object error: Blueprint not found.")
+    func _set_component_parameters(
+            new_component: Variant,
+            component_parameters: Dictionary,
+    ) -> void:
+        for parameter_name in component_parameters.keys():
+            if parameter_name == "texture":
+                component_parameters[parameter_name] = (
+                        SPRITES_PATH + component_parameters[parameter_name]
+                )
 
-		var new_game_object: GameObject = GameObject.new(blueprint_name)
-		var blueprint: Dictionary = blueprints.get(blueprint_name)
-		var blueprint_components: Dictionary = blueprint.get("components")
+            if parameter_name in new_component:
+                new_component[parameter_name] = component_parameters[parameter_name]
 
-		new_game_object.components = _create_components(blueprint_components)
 
-		return new_game_object
+    func _create_components(blueprint_components: Dictionary) -> Dictionary:
+        var components_class: Components = Components.new()
+        var components: Dictionary = {}
+
+        for idx in blueprint_components.size():
+            var current_component_name: String = blueprint_components.keys()[idx]
+            assert(
+                    current_component_name in components_class,
+                    "Create Components error: Component name not found.",
+            )
+
+            var current_parameters: Dictionary = blueprint_components.values()[idx]
+            var new_component: Variant = components_class[current_component_name].new()
+            _set_component_parameters(new_component, current_parameters)
+
+            components[current_component_name] = new_component
+
+        return components
+
+
+    func create_object(blueprint_name: String) -> GameObject:
+        assert(
+                self.blueprints.has(blueprint_name),
+                "Create Object error: Blueprint not found.",
+        )
+
+        var new_game_object: GameObject = GameObject.new(blueprint_name)
+        var blueprint: Dictionary = self.blueprints.get(blueprint_name)
+        var blueprint_components: Dictionary = blueprint.get("components")
+
+        new_game_object.components = _create_components(blueprint_components)
+
+        return new_game_object
 
 
