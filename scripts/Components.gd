fc6401a9dd26386b6851362ef615fcd48136a05c
Rain Babauta <rainbabauta1@gmail.com>
2023-12-18 21:42:58 -0800

    Start player-controlled movement
    adds PlayerControlled component and builds out Movable component

--- scripts/Components.gd
+++ scripts/Components.gd
@@ -3,21 +3,62 @@ extends GameEngine
 class_name Components
 
 
-class Physics extends Component:
-    enum Directions {N=1, S=-1, E=1, W=-1}
-
+class Movable extends Component:
     var speed: int = 0
-    var direction: Directions = Directions.N
+    var direction: String = "N"
+
+
+    func _move_forward(event: Event) -> void:
+        match self.direction:
+            "N":
+                self.game_object.rigidbody.global_translate(Vector2.UP * GameEngine.BASE_MOVE_SPEED)
+            "S":
+                self.game_object.rigidbody.global_translate(Vector2.DOWN * GameEngine.BASE_MOVE_SPEED)
+            "E":
+                self.game_object.rigidbody.global_translate(Vector2.RIGHT * GameEngine.BASE_MOVE_SPEED)
+            "W":
+                self.game_object.rigidbody.global_translate(Vector2.LEFT * GameEngine.BASE_MOVE_SPEED)
+
+
+    func _change_direction(event: Event) -> void:
+        self.direction = event.parameters.get("direction")
+
+
+    func fire_event(event: Event) -> Event:
+        if event.id == "MoveForward":
+            self._move_forward(event)
+        if event.id == "ChangeDirection":
+            self._change_direction(event)
+
+        return event
+
+
+class PlayerControlled extends Component:
+    func _init(game_object: GameObject = null) -> void:
+        super(game_object)
+        self.game_object.factory_from.subscribe(game_object, "player_controlled")
+
+
+    func fire_event(event: Event) -> Event:
+        if event.id == "ChangeDirection":
+            pass
+
+        return event
 
 
 class Render extends Component:
     var texture: String = ""
+    var sprite_node: Sprite2D
+
+
+    func first_time_setup() -> void:
+        self.sprite_node = Sprite2D.new()
+        sprite_node.texture = load(self.texture)
+        self.game_object.rigidbody.add_child(sprite_node)
+
 
     func fire_event(event: Event) -> Event:
-        if event.id == "CreateSpriteNode":
-            var sprite_node:= Sprite2D.new()
-            sprite_node.position = event.parameters.get("start_position")
-            sprite_node.texture = load(self.texture)
-            event.parameters.get("parent_node").add_child(sprite_node)
+        if event.id == "SetPosition":
+            self.game_object.rigidbody.global_position = event.parameters.get("position")
 
         return event
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,17 +1,30 @@
 extends Node
 
+const GameEngine = preload("res://scripts/GameEngine.gd")
+
+const TICK_THRESHOLD = 25
+var ticks: int = 0
+var game_object_factory: GameEngine.GameObjectFactory
+var john_goals_object: GameEngine.GameObject
+
+
+func _init() -> void:
+    game_object_factory = GameEngine.GameObjectFactory.new()
+    john_goals_object = game_object_factory.create_object("JohnGoals", self)
+
 
 func _ready() -> void:
-    generate_nodes()
-
-
-func generate_nodes() -> void:
-    var game_object_factory:= GameEngine.GameObjectFactory.new()
-    var john_goals_object: GameEngine.GameObject = game_object_factory.create_object("JohnGoals")
-
-    var create_sprite_node_event_parameters = {"parent_node": self, "start_position": Vector2(600, 300)}
-    var create_sprite_node_event:= GameEngine.Event.new(
-        "CreateSpriteNode",
-        create_sprite_node_event_parameters
+    var set_position_event_parameters = {"position": Vector2(600, 300)}
+    var set_position_event:= GameEngine.Event.new(
+        "SetPosition",
+        set_position_event_parameters
     )
-    john_goals_object.fire_event(create_sprite_node_event)
+    john_goals_object.fire_event(set_position_event)
+
+
+func _physics_process(delta: float) -> void:
+    ticks += 1
+    if ticks > TICK_THRESHOLD:
+        # TODO: if correct input: create event w/ said RAW input, to be processed by all player-controlled
+        john_goals_object.fire_event(GameEngine.Event.new("MoveForward"))
+        ticks = 0
