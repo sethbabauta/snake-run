2a2c35585f66aec19b3341ff792fa83e739bf0d3
Rain Babauta <rainbabauta1@gmail.com>
2024-01-30 10:48:04 -0800

    split out classic node from main

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,7 @@
 class_name Menu extends Control
 
-@export var classic_node: PackedScene
+@export var classic_scene: PackedScene
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(classic_node)
+	get_tree().change_scene_to_packed(classic_scene)
