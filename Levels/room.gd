09dd4dca07d364c785654a60b64a54407264322c
Rain Babauta <rainbabauta1@gmail.com>
2024-03-21 21:47:07 -0700

    room rework

--- Levels/room.gd
+++
@@ -1,4 +0,0 @@
-class_name Room extends Node2D
-
-@export var score_threshold: int
-@export var current_room_score: int = 0
---
+++ Levels/template/room.gd
@@ -0,0 +1,8 @@
+class_name Room extends Resource
+
+@export var score_threshold: int
+@export var tile_map: PackedScene
+@export var layout_x: int
+@export var layout_y: int
+
+var current_room_score: int = 0
