0d1bd3c71714056f759f536d4b67e98875f8ee83
Rain Babauta <rainbabauta1@gmail.com>
2024-04-08 14:20:47 -0700

    fix all rooms spawning bug

--- Scripts/dungeon.gd
+++ Scripts/dungeon.gd
@@ -12,38 +12,22 @@ var loaded_rooms: Array[Room]
 
 func _ready() -> void:
 	EventBus.level_changed.connect(_on_level_changed)
+	main_node.move_timer.speed_5.connect(_on_move_timer_speed_5)
 
 	_setup_initial_room()
 	EventBus.level_changed.emit("Start")
 
 
 func _load_room(room: Room) -> void:
-	var tile_map: RoomTileMap = room.tile_map.instantiate()
-	room_mapper.add_child(tile_map)
+	var room_tile_map: RoomTileMap = room.tile_map.instantiate()
+	room_mapper.add_child(room_tile_map)
+	room_tile_map.visible = false
 	var position_offset:= Vector2(room.layout_x * 20, room.layout_y * 20)
-	main_node.level_factory.setup_level(tile_map.room_tile_map, position_offset)
+	main_node.level_factory.setup_level(room_tile_map.room_tile_map, position_offset)
 	loaded_rooms.append(room)
 
 
-func _setup_initial_room() -> void:
-	current_room = room_mapper.get_room_at_layout_coordinates(Vector2(0, 0))
-	current_room_neighbors = room_mapper.get_room_neighbors(current_room)
-	_load_room(current_room)
-
-
-func _on_level_changed(direction: String) -> void:
-	match direction:
-		"North":
-			current_room = current_room_neighbors.up
-		"South":
-			current_room = current_room_neighbors.down
-		"East":
-			current_room = current_room_neighbors.right
-		"West":
-			current_room = current_room_neighbors.left
-
-	current_room_neighbors = room_mapper.get_room_neighbors(current_room)
-	# if room neighbors not loaded, load rooms
+func _load_room_neighbors() -> void:
 	if (
 		current_room_neighbors.left not in loaded_rooms
 		and current_room_neighbors.left
@@ -66,3 +50,34 @@ func _on_level_changed(direction: String) -> void:
 		_load_room(current_room_neighbors.down)
 
 
+func _setup_initial_room() -> void:
+	current_room = room_mapper.get_room_at_layout_coordinates(Vector2(0, 0))
+	current_room_neighbors = room_mapper.get_room_neighbors(current_room)
+	_load_room(current_room)
+
+
+func _on_level_changed(direction: String) -> void:
+	match direction:
+		"North":
+			current_room = current_room_neighbors.up
+		"South":
+			current_room = current_room_neighbors.down
+		"East":
+			current_room = current_room_neighbors.right
+		"West":
+			current_room = current_room_neighbors.left
+
+	current_room_neighbors = room_mapper.get_room_neighbors(current_room)
+	_load_room_neighbors()
+
+
+
+func _on_move_timer_speed_5() -> void:
+	# check if player head is on screen
+		# no:
+		# check direction and emit level changed
+		# return
+
+	# check if player tail is on screen
+		# emit player fully entered
+	pass
